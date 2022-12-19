e9b9f4a20491685158bc6234d2541177cec04851
Lê Sang <42503707+LeSangDeveloper@users.noreply.github.com>
2022-12-19 11:04:23 +0800

    init frame for Java native class VideoPlayerPlugin

--- android/src/main/java/com/example/video_player/VideoPlayerPlugin.java
+++ android/src/main/java/com/example/video_player/VideoPlayerPlugin.java
@@ -1,38 +1,140 @@
 package com.example.video_player;
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 
+import io.flutter.FlutterInjector;
+import io.flutter.Log;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.view.TextureRegistry;
 
 /** VideoPlayerPlugin */
-public class VideoPlayerPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
+public class VideoPlayerPlugin implements FlutterPlugin, Messages.AndroidVideoPlayerApi {
+  private FlutterState flutterState;
+  private final static String TAG = "VideoPlayerPlugin";
 
   @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "video_player");
-    channel.setMethodCallHandler(this);
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
-    }
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    final FlutterInjector injector = FlutterInjector.instance();
+    this.flutterState = new FlutterState(
+            binding.getApplicationContext(),
+            binding.getBinaryMessenger(),
+            injector.flutterLoader()::getLookupKeyForAsset,
+            injector.flutterLoader()::getLookupKeyForAsset,
+            binding.getTextureRegistry());
+    flutterState.startListening(this, binding.getBinaryMessenger());
   }
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
+    if (flutterState == null) {
+      Log.wtf(TAG, "Detached from the engine before registering to it.");
+    }
+    flutterState.stopListening(binding.getBinaryMessenger());
+    flutterState = null;
+    initialize();
+  }
+
+  @Override
+  public void initialize() {
+
+  }
+
+  @NonNull
+  @Override
+  public Messages.TextureMessage create(@NonNull Messages.CreateMessage msg) {
+    return null;
+  }
+
+  @Override
+  public void dispose(@NonNull Messages.TextureMessage msg) {
+
+  }
+
+  @Override
+  public void setLooping(@NonNull Messages.LoopingMessage msg) {
+
+  }
+
+  @Override
+  public void setVolume(@NonNull Messages.VolumeMessage msg) {
+
+  }
+
+  @Override
+  public void setPlaybackSpeed(@NonNull Messages.PlaybackSpeedMessage msg) {
+
+  }
+
+  @Override
+  public void play(@NonNull Messages.TextureMessage msg) {
+
+  }
+
+  @NonNull
+  @Override
+  public Messages.PositionMessage position(@NonNull Messages.TextureMessage msg) {
+    return null;
+  }
+
+  @NonNull
+  @Override
+  public byte[] getFrame(@NonNull Messages.PositionMessage msg) {
+    return new byte[0];
+  }
+
+  @Override
+  public void seekTo(@NonNull Messages.PositionMessage msg) {
+
+  }
+
+  @Override
+  public void pause(@NonNull Messages.TextureMessage msg) {
+
+  }
+
+  @Override
+  public void setMixWithOthers(@NonNull Messages.MixWithOthersMessage msg) {
+
+  }
+
+  private interface KeyForAssetFn {
+    String  get ( String  asset );
+  }
+
+  private interface KeyForAssetAndPackageName {
+    String get(String asset, String packageName);
+  }
+
+  private static final class FlutterState {
+    private final Context applicationContext;
+    private final BinaryMessenger binaryMessenger;
+    private final KeyForAssetFn keyForAsset;
+    private final KeyForAssetAndPackageName keyForAssetAndPackageName;
+    private final TextureRegistry textureRegistry;
+
+
+    private FlutterState(Context applicationContext, BinaryMessenger binaryMessenger, KeyForAssetFn keyForAsset, KeyForAssetAndPackageName keyForAssetAndPackageName, TextureRegistry textureRegistry) {
+      this.applicationContext = applicationContext;
+      this.binaryMessenger = binaryMessenger;
+      this.keyForAsset = keyForAsset;
+      this.keyForAssetAndPackageName = keyForAssetAndPackageName;
+      this.textureRegistry = textureRegistry;
+    }
+
+    void startListening(VideoPlayerPlugin methodHandler, BinaryMessenger messenger) {
+      Messages.AndroidVideoPlayerApi.setup(messenger, methodHandler);
+    }
+
+    void stopListening(BinaryMessenger messenger) {
+      Messages.AndroidVideoPlayerApi.setup(messenger, null);
+    }
+
   }
 }
